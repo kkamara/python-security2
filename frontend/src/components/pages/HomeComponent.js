ba7011648153c3cf4f87970c26253ffa3ff3a770
kkamara <kelvinkamara@protonmail.com>
2022-04-08 20:40:18 +0100

    Update page title

--- frontend/src/components/pages/HomeComponent.js
+++ frontend/src/components/pages/HomeComponent.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useNavigate, } from 'react-router-dom'
+import Helmet from 'react-helmet'
 
 export default function HomeComponent() {
   const navigate = useNavigate()
   
   return (
     <>
+      <Helmet>
+        <title>Django Boilerplate</title>
+      </Helmet>
       <div className='container'>
         <div className="col-md-6 offset-md-3 text-center">
           <h1>Test</h1>
